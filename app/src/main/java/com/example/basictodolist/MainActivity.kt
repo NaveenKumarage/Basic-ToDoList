c632c96d30073ad3e2472c68428591c15b1a0da9
Naveen Kumarage <dhakshitha.naveen119@gmail.com>
2024-05-13 11:18:04 +0530

    kotlin file changed

--- app/src/main/java/com/example/basictodolist/MainActivity.kt
+++ app/src/main/java/com/example/basictodolist/MainActivity.kt
@@ -27,7 +27,7 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             BasicToDoListTheme {
-                // A surface container using the 'background' color from the theme
+
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
